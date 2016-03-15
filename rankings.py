6d21b79e3ec87bb62e06d84501e2372aa7a91af1
Kevin Bernat <kevinbernat@cs.uchicago.edu>
2016-03-15 02:24:10 -0500

    cleaned up rankings.py

--- rankings.py
+++ rankings.py
@@ -10,9 +10,6 @@ import csv
 
 l_elec = coc_data.read_csv("Energy_Usage_2010_elec.csv")
 gas = coc_data.read_csv("Energy_Usage_2010_therms.csv")
-#census_elec = coc_data.read_csv("census_energy_data_elec.csv")
-#census_gas = coc_data.read_csv("census_energy_data_therms.csv")
-
 
 month_dict = {"jan":0, "feb":1, "mar":2, "apr":3, "may":4,
 "jun":5,"jul":6,"aug":7,"sep":8,"oct":9,"nov":10,"dec":11,"tot":12}
@@ -33,7 +30,7 @@ def determine_ranking(l,month):
 
     for key in d.keys():
         #Normalize by population
-        #print(l)
+
         if l == gas:
             total_pop_column = 14
         if l == l_elec:
@@ -62,11 +59,14 @@ def sort(filename,month):
             for key in neighborhood.keys():
                 if value == round(key,10):
                     rv.append(neighborhood[key])
-    #print(len(sorted_values))                
-    #print(len(rv))
+
     return rv
 
 def determine_census_ranking(l,month):
+    """
+    Given a filename and a month, returns a dictionary of the
+    energy usage for each census block normalized by population.
+    """
 
     ranking_list = {}
 
@@ -76,14 +76,14 @@ def determine_census_ranking(l,month):
         for row in reader:
             if float(row[14]) != 0:
                 ranking_list[round(float(row[month_dict[month]+1])/float(row[14]),10)] = row[0]
-
-    print(len(ranking_list))            
+           
     return ranking_list        
 
 def sort_census(filename,month):
     """
     Given a csv file of interest and a month, this ranks the neighborhoods
-    based on their electricity and gas consumption.
+    based on their electricity and gas consumption using a quicksort
+    algorithm.
     """
 
     blocks = determine_census_ranking(filename,month)
@@ -108,11 +108,6 @@ if __name__=="__main__":
     print(ans)
     print(ans2)
 
-    #ans3 = sort_census(l_elec,"tot")
-
-    #print(ans3)
-    #buildings = determine_building_ranking(l_elec,"tot")
-    #ans3 = sort(l_elec)
     ans4 = sort_census("census_energy_data_elec.csv","tot")
     print(len(ans4))
     ans5 = sort_census("census_energy_data_therms.csv","tot")
